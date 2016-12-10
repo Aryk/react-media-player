c72375d0e10cb74099a0f0db1129a41fcdbbbe9e
Aryk Grosz <aryk.grosz@gmail.com>
2016-12-11 00:48:28 +0200

    Small adjustments

--- src/Player.jsx
+++ src/Player.jsx
@@ -128,9 +128,9 @@ class Player extends Component {
     _mediaGetters.getPlayerEvents.onTimeUpdate(currentTime)
 
     if (this._vendor !== 'youtube') {
-      // Aryk: Added 0.1 to anticipate the next keyframe
+      // @Aryk: Added 0.1 to anticipate the next keyframe
       if (endTime && currentTime + 0.1 > endTime) {
-        // Aryk: For Youtube, stop() will also trigger onEnded, but
+        // @Aryk: For Youtube, stop() will also trigger onEnded, but
         // we have Youtube as an exception, so we are ok here.
         media.stop();
         this._handleOnEnded();
@@ -147,7 +147,7 @@ class Player extends Component {
     // internet connection and are not receiving new data, so we must let this loop continue.
     // Putting this on the Player level allows this behavior to apply to other sources like Vimeo
     // as well and provides for consistent treatment.
-    if (progress > (this._lastProgress || 0)) {
+    if (progress > (this._lastProgress || -1)) { // -1 so it's true first time around.
       _mediaGetters.getPlayerEvents.onProgress(progress)
     }
 

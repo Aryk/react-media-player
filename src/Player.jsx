64ed6b0c0a282853e4aa95dec3ea8140b0ec4028
Aryk Grosz <aryk.grosz@gmail.com>
2017-01-06 22:22:53 +0200

    Add helper methods to mediaHelper

--- src/Player.jsx
+++ src/Player.jsx
@@ -42,7 +42,7 @@ class Player extends Component {
     // Must passed in 'last_' through props to actually get these states changed.
     setSeekTo: PropTypes.number,
     setSkipTime: PropTypes.number,
-    setPauseOnceAt: PropTypes.number,
+    setPauseAt: PropTypes.number,
     setPlayback: PropTypes.oneOf(['playing', 'paused']),
     setVolume: PropTypes.number,
     setAddVolume: PropTypes.number,
@@ -59,7 +59,7 @@ class Player extends Component {
     // These must be passed in through props in order to initiate a state change. Changes to these will trigger actions.
     setSeekTo : null,
     setSkipTime: null,
-    setPauseOnceAt: null,
+    setPauseAt: null,
     setPlayback: 'paused',
     setVolume: 1,
     setAddVolume: null,
@@ -162,13 +162,15 @@ class Player extends Component {
     let actionPerformed = false;
 
     // pauseAt doesn't trigger anything here, happens while item is playing...
+    // order is important here. For example, if you have seekTo and then play after, we should make sure that that
+    // seekTo comes first, for example.
     [
       ['seekTo', 'number'],
       ['skipTime', 'number'],
       ['playback', 'string'],
+      ['mute', 'boolean'],
       ['volume', 'number'],
       ['addVolume', 'number'],
-      ['mute', 'boolean'],
       ['fullscreen', 'boolean'],
     ].forEach((args) => {
         let [actionName, varType] = args;
@@ -179,7 +181,7 @@ class Player extends Component {
           actionPerformed = true;
           this._debug(`Performing action ${actionName}: `, newPropValue);
           this.componentSetters[actionName](newPropValue); // execute the action!
-          this.setMediaState({[setterName]: null}) // clear it out from the state since we executed it!
+          this.setMediaState({[setterName]: null}); // clear it out from the state since we executed it!
         }
       }
     );
@@ -192,7 +194,14 @@ class Player extends Component {
   componentSetters = {
     mute: (setMute) => this._component.mute(setMute),
     volume: (volume) => this._component.setVolume(volume),
-    seekTo: (currentTime) => this._component.seekTo(currentTime),
+    seekTo: (currentTime) => {
+      this.ignoreUntilSeekToStarts = currentTime;
+      // FAIL SAFE: Make sure to kill this in the event we can't detect successfully when it picks up from where it
+      // left off.
+      setTimeout(() => this.ignoreUntilSeekToStarts = null, 1000);
+      this._component.seekTo(currentTime);
+
+    },
     playback: (playback) => {
       switch (playback) {
         case 'playing':
@@ -245,6 +254,25 @@ class Player extends Component {
     }
   };
 
+    // @Aryk: Basically what is happening here is that on some players (ie Youtube), after you seek, you still
+    // get a few key frames from finishing where it was at before. So the currentime will look like this:
+    //
+    //   3, 3.1, 3.2 (then seekTo(5)), 5, 3.3, 5.1, 5.2, 5.3...
+    //
+    // So you have a 3.3 in there that shouldn't really be there, so we will ignore it. We use a buffer of 0.2, so we
+    // wait until the readings start being inside the
+  _ignoreUntilSeekToCatchesUp = currentTime => {
+    let ignore = false;
+    if (this.ignoreUntilSeekToStarts) {
+      if (this.ignoreUntilSeekToStarts < currentTime && currentTime < (this.ignoreUntilSeekToStarts + 0.5)) {
+        this.ignoreUntilSeekToStarts = null;
+      } else {
+        ignore = true;
+      }
+    }
+    return ignore;
+  };
+
   // Component callbacks are responsible for updating all the mediaState 'stat*" keys AND running the passed in callbacks
   // from the user.
   componentCallbacks = {
@@ -277,19 +305,24 @@ class Player extends Component {
 
     onDuration: (statDuration) =>  this._updateStatAndRunCallback({statDuration}, 'onDuration', statDuration),
     onTimeUpdate: (statCurrentTime) => {
-      const { setPauseOnceAt, endTime } = this.props;
+      const { setPauseAt, endTime } = this.props;
       // @Aryk: Added 0.1 to anticipate the next keyframe
-      const anticipateNextFrame = statCurrentTime + 0.1 ;
+      const anticipateNextFrame = statCurrentTime + 0.1;
+
+      if (this._ignoreUntilSeekToCatchesUp(statCurrentTime)) {
+        return;
+      }
 
       this._updateStatAndRunCallback({statCurrentTime}, 'onTimeUpdate', statCurrentTime);
 
+      // Youtube has stop time implemented.
       if (this._vendor !== 'youtube' && endTime && anticipateNextFrame > endTime) {
         // @Aryk: For Youtube, stop() will also trigger onEnd, but
         // we are not in this block if we are on youtube.
         this._component.end(); // will trigger onEnd callbacks
       }
 
-      if (setPauseOnceAt && anticipateNextFrame > setPauseOnceAt) {
+      if (setPauseAt && anticipateNextFrame > setPauseAt) {
         this._component.pause();
       }
     },
@@ -309,15 +342,7 @@ class Player extends Component {
 
     onLoad: () => this._updateStatAndRunCallback({statPlayback: 'loading'}, 'onLoad'),
     onPlay: () => this._updateStatAndRunCallback({statPlayback: 'playing'}, 'onPlay'),
-    onPause: () => {
-      let statUpdate = {statPlayback: 'paused'};
-
-      if (this.props.setPauseOnceAt) {
-        Object.assign(statUpdate, {setPauseOnceAt: null})
-      }
-
-      this._updateStatAndRunCallback(statUpdate, 'onPause')
-    },
+    onPause: () => this._updateStatAndRunCallback({statPlayback: 'paused'}, 'onPause'),
     onEnd: () => {
       const { loop, onEnd, startTime } = this.props;
 
